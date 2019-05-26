f310df58ae3af4eb86aeafa0da8b21be7b426f52
eduardofagnoni <eduardofagnoni@gmail.com>
2019-05-26 14:21:02 -0300

    inclusão de métodos na classe usuario

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
 <?php
 require_once("config.php");
 
+/**Carrega um usuario */
+/*
 $root = new Usuario();
-
 $root->loadById(1);
-
 echo $root;
+*/
+
+/**carrega uma lista de usuarios */
+/*
+echo "<br><br>";
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+/**carrega lista de usuarios buscando pelo login */
+/*
+echo "<br><br>";
+$search = Usuario::search("eduardo");
+echo json_encode($search);
+*/
+
+
+/**Carrega use autenticado */
+echo "<br><br>";
+$usuario = new Usuario();
+$usuario->login("eduardo","asas123");
+echo ($usuario);
+
 
 ?>
